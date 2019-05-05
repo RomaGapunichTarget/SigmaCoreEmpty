1ba4daff91b46a2da2e2c807f569eac64eb7e041
Roma <romagapunich@gmail.com>
2019-05-05 20:31:43 +0300

    logs console and file

---
+++ SigmaCoreEmpty/FileLoggerProvider.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
+
+namespace SigmaCoreEmpty
+{
+    public class FileLoggerProvider
+        : ILoggerProvider
+    {
+        private string path;
+        public FileLoggerProvider(string _path)
+        {
+            path = _path;
+        }
+        public ILogger CreateLogger(string categoryName)
+        {
+            return new FileLogger(path);
+        }
+
+        public void Dispose()
+        {
+        }
+    }
+    public static class FileLoggerExtensions
+    {
+        public static ILoggerFactory AddFile(this ILoggerFactory factory,
+            string filePath)
+        {
+            factory.AddProvider(new FileLoggerProvider(filePath));
+            return factory;
+        }
+    }
+}
--- SigmaCoreEmpty/Startup.cs
+++ SigmaCoreEmpty/Startup.cs
@@ -1,34 +1,64 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
 
 namespace SigmaCoreEmpty
 {
     public class Startup
     {
+        IHostingEnvironment _env;
+        public Startup(IHostingEnvironment env)
+        {
+            _env = env;
+        }
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
         {
+            loggerFactory.AddFile(Path.Combine(Directory.GetCurrentDirectory(), "logger.txt"));
+            var loggerFile = loggerFactory.CreateLogger("FileLogger");
+            loggerFactory.AddConsole();
+            // если приложение в процессе разработки
             if (env.IsDevelopment())
             {
+                // то выводим информацию об ошибке, при наличии ошибки
                 app.UseDeveloperExceptionPage();
             }
 
+            //app.Run(async (context) =>
+            //{
+            //    await context.Response.WriteAsync("Hello World!");
+            //});
             app.Run(async (context) =>
             {
-                await context.Response.WriteAsync("Hello World!");
+                loggerFile.LogInformation("Processing request {0}", context.Request.Path);
+                // создаем объект логгера
+                var logger = loggerFactory.CreateLogger("RequestInfoLogger");
+                // пишем на консоль информацию
+                logger.LogInformation("Processing request {0}", context.Request.Path);
+
+                await context.Response.WriteAsync(_env.ApplicationName);
+
             });
         }
+
+        private void ServiceLogs()
+        {
+
+        }
+
     }
 }
